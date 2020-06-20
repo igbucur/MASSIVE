7a28197173c2a3e768af39faf604e7c4e511d328
igbucur <ioan.gabriel.bucur@gmail.com>
2020-06-20 13:18:56 +0200

    renamed MASSIVE.R to MASSIVE-package.R

--- R/MASSIVE-package.R
+++ R/MASSIVE-package.R
@@ -1,8 +1,11 @@
 #' MASSIVE: Model Assessment and Stochastic Search for Instrumental Variable Estimation
 #' 
+## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
+## usethis namespace: end
 #' @useDynLib MASSIVE
 #'
 #' @docType package
 #' @name MASSIVE
 NULL
+
